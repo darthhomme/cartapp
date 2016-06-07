9cc0913fa50c52c2fe23683c725e8f281fde257a
Jeong Kim <joshkim83@outlook.com>
2016-06-07 17:04:44 -0600

    added comments and stuff in page controller above any action

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,7 @@
 class PageController < ApplicationController
+  before_action :authenticate_user!, except: [:home, :about, :faqs]
+  # another way of doing above line is: before_action :authenticate_user!, only: [:faqs]
+  # but it all depends on your preference and how much clean code you want to write
   def home
   end
 
