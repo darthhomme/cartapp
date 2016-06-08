0b9f136b53d24f8606f8985decdf98fef35661e3
Jeong Kim <joshkim83@outlook.com>
2016-06-07 23:15:47 -0600

    just seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Product.delete_all
+#
+# Product.create!([
+#   {id: 1, name: "Nintendo Wii U Premium", price: 250, desc: true},
+#   {id: 2, name: "XBox 360 250GB", price: 250, active: true},
+#   {id: 3, name: "Playstation 3 500 GB", price: 239.95, active: true},
+#   {id: 4, name: "Nintendo Wii", price: 99.95, active: true},
+#   {id: 5, name: "Nintendo 3DS", price: 174.95, active: true}
+# ])
+
+Product.create("name": "Trustforge Auto Tag Mount", "brand": "Brions", "model": "X7", "sku": "13180a6d-86e0-408c-a5c0-28254bd97720", "price": 281, "desc": "Et sed quis eligendi non ut et sunt eveniet. Atque necessitatibus aliquam et soluta. Commodi quo molestiae dolor est repudiandae quos error asperiores. Fuga quod aperiam laborum dolores.")
+Product.create("name": "Onenix Air Component", "brand": "Ambalt", "model": "OR-3186", "sku": "024f8ec0-193e-4897-8b3c-1b315691a3b5", "price": 126, "desc": "Harum aut autem magni earum. Tenetur quod tempore aut expedita. Itaque consequatur et laborum blanditiis eius magnam exercitationem. Nihil sequi sed minus ipsam sed. Rerum enim sint quisquam voluptas.")
+Product.create("name": "Socell Video Adapter", "brand": "Brens", "model": "S86", "sku": "0d4e913a-8d3c-4c8c-b673-99744fb213e4", "price": 101, "desc": "Omnis repellendus dolore atque eligendi quae id. Numquam quia voluptate sequi quae itaque sapiente. Ut dolores et fuga enim asperiores iure consequuntur.")
+Product.create("name": "Phiont Electric GPS Filter", "brand": "Bruck", "model": "SK-3997", "sku": "7089eebe-fe0c-42c2-bb38-0f36b29e187c", "price": 103, "desc": "Non tempora perferendis a nesciunt perspiciatis. Et non est ea laborum. Laudantium deleniti saepe aut placeat est.")
+Product.create("name": "KYJ Tag HD Component", "brand": "Ampod", "model": "X27", "sku": "1f59a280-ccca-45ba-82e1-11e83794e8f0", "price": 263, "desc": "Corrupti quos sed asperiores quis labore voluptas minima. Quam ipsam magnam non sunt voluptate omnis. Doloribus voluptatum magni et placeat suscipit. Iste voluptatibus harum quas odit. Quisquam mollitia earum ipsa voluptatum.")
+Product.create("name": "Techbalt Gel Output Bracket", "brand": "Onesche", "model": "H-7927", "sku": "9189cfe2-8584-4ee0-80b0-348df8ba8f2e", "price": 122, "desc": "Ex et qui ea possimus molestias. Adipisci tenetur quas rerum magni. Voluptas dicta maxime maiores natus magnam ut rerum recusandae.")
+Product.create("name": "Subpod Side Component", "brand": "Aquacell", "model": "G-5085", "sku": "4fa3a03d-c326-4f9e-bc2c-f8e69aaba155", "price": 122, "desc": "Cum occaecati placeat aliquam amet dolorem accusamus. Accusantium ipsam qui aut ut est dolor laborum. Rerum voluptates deserunt aut laudantium et alias. Officia deserunt illo ratione et sunt ea consectetur fugiat. Aut aut nulla distinctio aspernatur.")
+Product.create("name": "Briensforge Side Digital Filter", "brand": "Phinceforge", "model": "D-6667", "sku": "e5e5b807-bb34-4efc-bcc5-bf48ce53a862", "price": 213, "desc": "Illum voluptates nulla nihil sequi qui blanditiis at est. Molestias occaecati et aut quod. Et voluptatibus architecto dolorem nulla. Voluptatem rerum ipsa dolorum iste cumque dolor.")
+Product.create("name": "Phene Disc Kit", "brand": "Pieffe", "model": "Y78", "sku": "eede4108-421c-4d7b-82a2-db4d6e078844", "price": 177, "desc": "Nulla molestiae exercitationem delectus magni est. Et facilis sit consequatur optio enim qui numquam. Optio porro sit in quidem ut minima harum illum. Qui ut soluta atque corrupti et vel.")
+Product.create("name": "Onepod Digital Amplifier", "brand": "Onepod", "model": "G55", "sku": "5f5eaeb8-9fa4-49ec-a8ad-1434cb9732ef", "price": 146, "desc": "Hic odio ex voluptates eos voluptatem. Voluptate et quia corporis modi adipisci ipsum et a. Non dolorem excepturi commodi tempore eos.")
+Product.create("name": "Subsync Tag Bridge", "brand": "Panasche", "model": "A-9192", "sku": "e34513e4-9928-4c35-b4a7-409e7ccaafee", "price": 176, "desc": "Et ducimus optio ut maxime distinctio enim neque voluptatibus. Voluptate ipsum fugiat eum neque qui corrupti nihil ut. Aliquid natus voluptatem est et ipsum saepe.")
+Product.create("name": "Trynce Output Mount", "brand": "Brons", "model": "W56", "sku": "eb7dbfe4-263a-4a13-aeb1-8655c0f6c91a", "price": 102, "desc": "Qui libero delectus nihil autem ad. Eligendi dolorem eos autem aspernatur laudantium qui iste reprehenderit. Qui sit eveniet in magni fugit nesciunt. Ut ipsam repudiandae quas quaerat suscipit exercitationem. Nisi itaque nostrum eum aut laudantium repellendus et.")
+Product.create("name": "Phyckforge Direct Gel Component", "brand": "Cygfunc", "model": "MN-5714", "sku": "8e9ad752-9ba9-44aa-b8da-f228e61aba88", "price": 181, "desc": "Illum vel ut et unde fugiat possimus quia quos. Officiis quibusdam qui esse fuga laudantium provident. Fugit officiis natus ipsa adipisci ut quos ad eius.")
+Product.create("name": "Pyrewood GPS Audible Receiver", "brand": "Pyncefunc", "model": "Y-3912", "sku": "e8d410a7-1314-40e6-bfff-23986f9e5a86", "price": 252, "desc": "Autem fuga occaecati magnam sint harum dolorem. Nisi nihil soluta dolor hic aliquid vero et unde. In et nisi quasi et eveniet voluptatem velit. Nostrum facilis unde voluptatem et repellendus voluptate culpa. In natus voluptatem fuga inventore.")
+Product.create("name": "Pinefunc Performance GPS Case", "brand": "Phince", "model": "H-6831", "sku": "74c1cd80-b1db-48ba-b67b-39b44bbf9d42", "price": 153, "desc": "Itaque nesciunt aliquam inventore quasi ut id. Et illum facilis et magni quia. Aut molestias quis eius quibusdam rem consequuntur magnam.")
+Product.create("name": "WFE Audible Tuner", "brand": "Phyph", "model": "U49", "sku": "941f75d7-7d7e-4095-9e09-707452b73d2b", "price": 208, "desc": "Velit quisquam dolorem et et perspiciatis atque. Velit et amet ea quisquam ipsa qui facere quia. Tenetur sunt a eligendi tempore adipisci sequi dolorem eos.")
+Product.create("name": "Tricell Tag Output Filter", "brand": "Pounsforge", "model": "E77", "sku": "3111332b-1adb-4452-b4ae-dc2194ec7c77", "price": 161, "desc": "Reiciendis quos rerum eos dignissimos. Quis dolorem dolorum odio porro quo quod reprehenderit aut. Animi in vel sed distinctio qui laborum soluta ex.")
+Product.create("name": "Phent Disc Output Tuner", "brand": "Poncefunc", "model": "X-2350", "sku": "3d454af5-5de4-4801-b8ac-b4f6296f20d5", "price": 136, "desc": "Commodi iusto eligendi sit quasi. Esse sequi molestiae deleniti nisi. Aut omnis dolor quia error.")
+Product.create("name": "Subsche Digital Controller", "brand": "Trast", "model": "T57", "sku": "4f774861-59fb-452e-870a-a5b174c65c54", "price": 283, "desc": "Excepturi sit non iste et debitis ut eveniet. Unde vero quia exercitationem dolore occaecati corporis qui amet. Molestias beatae porro quia quia ratione dolorem.")
+Product.create("name": "Pophwood Remote Bracket", "brand": "Techfunc", "model": "P-6686", "sku": "619e3330-4db6-4487-9ea6-263891480627", "price": 122, "desc": "Cum quis fugiat sed et. Explicabo sequi quisquam quasi soluta similique rerum et. Labore nesciunt adipisci cupiditate voluptatem.")
+Product.create("name": "Bryst Audible Case", "brand": "Briph", "model": "F-9445", "sku": "cee84e4f-acf1-4f42-8487-fb7dd99f21f7", "price": 276, "desc": "Suscipit sed nam aut voluptatem. Illo architecto dolor in soluta temporibus officia. Quia aliquid impedit repudiandae neque hic. Enim nisi corrupti fuga magnam quaerat necessitatibus similique.")
+Product.create("name": "Phuns Portable Tuner", "brand": "Triph", "model": "ZL-5456", "sku": "4fc4bfb9-2aaf-41f3-88da-ca925157ac24", "price": 136, "desc": "Quae pariatur non consequatur aut sit eos. Minima omnis voluptate recusandae quibusdam voluptas laborum eos. Eos labore ea culpa corporis eaque aut sunt. Assumenda quae ex ut velit autem et saepe ut. Rerum consectetur sed atque officia.")
+Product.create("name": "Aquacell Digital Compressor", "brand": "Aftersche", "model": "D9", "sku": "62235bab-de9a-43bd-b9a1-1f1f41a2636e", "price": 299, "desc": "A pariatur et vel eaque aut et. Pariatur sapiente praesentium aliquam officiis quia. Est molestiae doloremque ea incidunt qui et illum quia. Qui dolorum odit fuga rerum quia eveniet.")
+Product.create("name": "UPM Output HD Adapter", "brand": "Phynewood", "model": "BG-1915", "sku": "57d0a84e-2cf3-44ec-9d79-18b103145c2c", "price": 128, "desc": "Ratione error natus reprehenderit culpa consequuntur. Quas ipsa aut expedita voluptatem id consequatur consequatur. Est temporibus tempora sint iste est. Eligendi vel beatae ipsam voluptatem soluta laborum quidem. Aut et aliquid odio tempora.")
+Product.create()
+Product.create()
+Product.create()
+Product.create()
+Product.create()
+Product.create()
