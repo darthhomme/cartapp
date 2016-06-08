e5d3c52e293670b74ffae85a65ce00a84f66ae23
Jeong Kim <joshkim83@outlook.com>
2016-06-08 07:49:07 -0600

    added some comments to cart controller to better explain thinking

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -11,7 +11,7 @@ class CartController < ApplicationController
         cart = session[:cart]
       end
 
-      if cart[id] then        # if the product has been added to the cart, the increase by 1.
+      if cart[id] then        # if the product has been added to the cart, then increase by 1.
         cart[id] = cart[id] + 1
       else
         cart[id] = 1
@@ -24,7 +24,16 @@ class CartController < ApplicationController
     redirect_to :action => :index
   end
 
-  def index
+
+  # def minusCart I have to confess, I couldn't figure this action out. I will. I want to take items off
+  #   if cart[id] then         if the product has been added to the cart, then subtract by 1.
+  #     cart[id] = cart[id] -= 1
+  #   end
+  #
+  # end
+
+
+  def index #when user goes to the /cart then the cart should be empty or full of some product.
     @product = Product
     if session[:cart] then
       @cart = session[:cart]
