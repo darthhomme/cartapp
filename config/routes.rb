c60e486fe807dfa3e139d83e80c751e418f1637f
Jeong Kim <joshkim83@outlook.com>
2016-06-09 12:19:26 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ Rails.application.routes.draw do
   get '/cart/clear' => 'cart#clearCart'
   get '/cart/:id' => 'cart#add'
 
-
-  get 'users/sign_out' => 'cart#index'
-
   resources :products
   root 'page#home'
   # root 'welcome#index'
