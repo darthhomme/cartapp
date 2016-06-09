254893bdca66b0f05bcf7b4807f59b306613dc13
Jeong Kim <joshkim83@outlook.com>
2016-06-09 06:36:53 -0600

    added precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( page.scss )
 Rails.application.config.assets.precompile += %w( products.css )
 Rails.application.config.assets.precompile += %w( scaffolds.css )
-# Rails.application.config.assets.precompile += %w( welcome.scss )
+Rails.application.config.assets.precompile += %w( products.js)
+Rails.application.config.assets.precompile += %w( welcome.scss )
